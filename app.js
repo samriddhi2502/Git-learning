b1398b410c8afade66b880e6a87cb79b6aad0857
Samriddhi Bansal <bansalsamriddhi335@gmail.com>
2025-12-28 11:36:33 +0530

    Second commit

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const name = "Sam";
 const age = 24;
 let balance = 420;
 
+// updating
+
 function update_balance(balance){
     let value = 30;
     balance+=value;
 }
+
+// login page 
+
+function login(){
+    console.log("Login button created");
+}
+
+
 
