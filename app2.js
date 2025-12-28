47312643c3cb55970407d323ba4c8217a6e6cdc8
Samriddhi Bansal <bansalsamriddhi335@gmail.com>
2025-12-28 12:37:50 +0530

    Payment added

--- app2.js
+++ app2.js
@@ -7,3 +7,8 @@ console.log(button);
 
 const login = "Login page added"
 console.log("login");
+
+// payment branch creation
+
+const payment = "Integrated the payment gateway";
+console.log("payment");
